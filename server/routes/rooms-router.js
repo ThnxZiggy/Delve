70f331d4f174e7238f8b8fb1bfca05fefe9ab453
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-06-13 00:05:26 +0000

    all components using database calls for users

--- server/routes/rooms-router.js
+++ server/routes/rooms-router.js
@@ -3,29 +3,6 @@ const { route } = require('./todos-router');
 const router = express.Router();
 const db = require('../configs/db.config');
 
-const rooms = {
-  a: {
-    id: "a",
-    name: "sewing",
-    members: [],
-  },
-  b: {
-    id: "b",
-    name: "dancing",
-    members: [],
-  },
-  c: {
-    id: "c",
-    name: "regex",
-    members: [],
-  },
-  d: {
-    id: "d",
-    name: "drawing",
-    members: [],
-  },
-}
-
 router.get('/', (req, res) => {
   const command = "SELECT * FROM rooms";
   db.query(command).then(data => {
