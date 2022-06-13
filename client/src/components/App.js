dd42cf0b6980fe4c29737245f8382151cfcbdadb
NadyaCodes <nadyacorscadden@gmail.com>
2022-06-13 18:30:07 -0400

    add first confetti attempt

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,10 +8,13 @@ import Nav from './Nav';
 import Sidebar from './Sidebar';
 
 
+
+
 // all we need to do is CONNECT to the backend socket server!
 // STEP 0: Install socket.io-client <-- THE VER OF SERVER SOCKET.IO MUST MATCH
 // STEP 1: import socket.io-client
 import io from 'socket.io-client';
+import confetti from 'canvas-confetti';
 // STEP 2: make the connection
 const socketURL =
   process.env.NODE_ENV === 'production'
@@ -21,6 +24,22 @@ const socketURL =
 const socket = io.connect(socketURL, {secure: true});
 
 // all listeners of socket.io we build will be inside of the useEffect
+
+//confetti
+require('canvas-confetti');
+// const myCanvas = document.createElement('canvas');
+// document.body.appendChild(myCanvas);
+
+// const myConfetti = confetti.create(myCanvas, {
+//   resize: true,
+//   useWorker: true
+// });
+// myConfetti({
+//   particleCount: 1500,
+//   spread: 1600
+//   // any other options from the global
+//   // confetti function
+// });
 
 function App() {
   const [state, setState] = useState({
@@ -39,13 +58,94 @@ function App() {
       })
   }, [state.user])
 
+//   party(event) {
+//     setTheme('App party');
+//     myConfetti();
+//  }
+
+
+
+   const partyConfetti = () => {
+     
+    var end = Date.now() + (15 * 400);
+    
+    (function frame() {
+      //bottom
+      confetti({
+        // startVelocity: 55,
+        particleCount: 2,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0, y:1 },
+      });
+      confetti({
+        // startVelocity: 55,
+        particleCount: 2,
+        angle: 120,
+        spread: 55,
+        origin: { x: 1, y:1 },
+      });
+
+      //top corners
+      confetti({
+        startVelocity: 30,
+        particleCount: 2,
+        angle: 120,
+        spread: 55,
+        origin: { x: 1, y: 0 },
+      });
+
+      confetti({
+        startVelocity: 30,
+        particleCount: 2,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0, y: 0 },
+      });
+
+      //top middle
+      confetti({
+        startVelocity: 30,
+        particleCount: 2,
+        angle: 120,
+        spread: 55,
+        origin: { x: .3, y: 0 },
+      });
+
+      confetti({
+        startVelocity: 30,
+        particleCount: 2,
+        angle: 120,
+        spread: 55,
+        origin: { x: .87, y: 0 },
+      });
+
+      // confetti({
+      //   particleCount: 2,
+      //   angle: 120,
+      //   spread: 55,
+      //   origin: { y: 0 },
+      // });
+
+      // requestAnimationFrame(frame)
+    
+      if (Date.now() < end) {
+        requestAnimationFrame(frame);
+      }
+    }());
+
+    }
+
+
+
   return (
     <div className={theme}>
       <span className="theme__icons">
       <i className="fa-solid fa-sun fa-3x" onClick={() => setTheme('App light')}></i>
-      <i className="fa-solid fa-moon fa-3x" onClick={() => setTheme('App dark')}></i>
-      <i className="fa-solid fa-cake-candles fa-3x" onClick={() => setTheme('App party')}></i>
+      <i className="fa-solid fa-moon fa-3x" onClick={() => {setTheme('App dark'); confetti.reset()}}></i>
+      <i className="fa-solid fa-cake-candles fa-3x" onClick={() => {setTheme('App party'); partyConfetti()}}></i>
       </span>
+
       {state.user.name && <Nav socket={socket} user={state.user} onClick={setState} state={state}/>}
       <header className="App-header" style={{display: 'flex',}}>
         {state.user.name&& !state.makingRoom && <Sidebar socket={socket} user={state.user} onClick={setState} state={state}/>}
