1c1fdcc69762ee917fd1d817cc7630ce12b40427
NadyaCodes <nadyacorscadden@gmail.com>
2022-06-15 12:51:10 -0400

    add styling to about us page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -8,10 +8,12 @@ export default function About({setState}) {
   console.log("hello world");
 
   return (
-    <div> 
-      <button onClick={exit}>Exit</button>
+    <div className="about-container"> 
+            <button onClick={exit} className='close'>&#x2718;</button>
+    <div className="about-us"> 
+
   
-      <div>
+      <div className="profile">
         <img 
           src="https://media-exp2.licdn.com/dms/image/C5603AQEIIyxwFiXsCQ/profile-displayphoto-shrink_800_800/0/1573512899213?e=1660780800&v=beta&t=r-sj0JkzhebtlbLwBnlFUamMq2htt9SpFaUGp0JTYTU" 
           alt="Nadya profile picture" 
@@ -20,7 +22,7 @@ export default function About({setState}) {
         />
         <h1>Nadya Corscadden</h1>
         <h3>Full-stack Developer</h3>
-        <div>
+        <div className='profile-links'>
           <a href="https://www.linkedin.com/in/nadya-corscadden-34222054/" target="_blank" rel="noopener noreferrer">
             <ion-icon name="logo-linkedin"></ion-icon>
           </a>
@@ -30,7 +32,7 @@ export default function About({setState}) {
         </div>
       </div>
 
-      <div>
+      <div className="profile">
         <img 
           src="https://media-exp2.licdn.com/dms/image/C4E03AQFYv34RamatyQ/profile-displayphoto-shrink_800_800/0/1524095683018?e=1660780800&v=beta&t=vcZPdpNqCiBtIOfaJzANRJcbIw3CmY1WeC7-9IQauXo" 
           alt="Ziggy profile picture" 
@@ -39,7 +41,7 @@ export default function About({setState}) {
         />
         <h1>Manmohit Matharu</h1>
         <h3>Full-stack Developer</h3>
-        <div>
+        <div className='profile-links'>
           <a href="https://www.linkedin.com/in/manmohitmatharu/" target="_blank" rel="noopener noreferrer">
             <ion-icon name="logo-linkedin"></ion-icon>
           </a>
@@ -49,7 +51,7 @@ export default function About({setState}) {
         </div>
       </div>
 
-      <div>
+      <div className="profile">
         <img 
           src="https://media-exp2.licdn.com/dms/image/C5603AQEjnv1VZqPSjw/profile-displayphoto-shrink_800_800/0/1653239110445?e=1660780800&v=beta&t=OqeuBKnFHReWA6D_1x9l2gIQZFqp2e3NeK7GnwRmjUA" 
           alt="Nathan profile picture" 
@@ -58,7 +60,7 @@ export default function About({setState}) {
         />
         <h1>Nathan Lunn</h1>
         <h3>Full-stack Developer</h3>
-        <div>
+        <div className='profile-links'>
           <a href="https://www.linkedin.com/in/nathan-lunn-06a1a7225/" target="_blank" rel="noopener noreferrer">
             <ion-icon name="logo-linkedin"></ion-icon>
           </a>
@@ -67,7 +69,7 @@ export default function About({setState}) {
           </a>
         </div>
       </div>
-
+    </div>
     </div>
   )
 }
