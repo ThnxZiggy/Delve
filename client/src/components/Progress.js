ef0958bb7414b9bb9335c51e8e60ec15379ab41d
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-06-17 03:40:58 +0000

    fixed both > than 100 and float percentage bugs on Progress

--- client/src/components/Progress.js
+++ client/src/components/Progress.js
@@ -6,17 +6,29 @@ export default function Progress({state}) {
 
   return (
     <div>
-      { state.sessionComplete ?
-      <div>
-        <CircleProgress percentage={(state.room.session_number + 1) / state.room.session_goal * 100} strokeWidth={8} />
-        <h5>Sessions Completed: {state.room.session_number + 1}</h5>
+    {state.room.session_number >= state.room.session_goal
+    ? 
+    <div>
+      <CircleProgress percentage={100} strokeWidth={8} />
+          <h5>Sessions Completed: {state.sessionComplete ? state.room.session_number + 1 : state.room.session_number}</h5>
+    </div>
+    :
+    <div>
+        <div>
+        { state.sessionComplete ?
+        <div>
+          <CircleProgress percentage={Math.floor((state.room.session_number + 1) / state.room.session_goal * 100)} strokeWidth={8} />
+          <h5>Sessions Completed: {state.room.session_number + 1}</h5>
+        </div>
+        :
+        <div>
+          <CircleProgress percentage={Math.floor(state.room.session_number / state.room.session_goal * 100)} strokeWidth={8} />
+          <h5>Sessions Completed: {state.room.session_number}</h5>
+        </div>
+        }
       </div>
-      :
-      <div>
-        <CircleProgress percentage={state.room.session_number / state.room.session_goal * 100} strokeWidth={8} />
-        <h5>Sessions Completed: {state.room.session_number}</h5>
-      </div>
-      }
+    </div>
+    }
     </div>
   )
 }
