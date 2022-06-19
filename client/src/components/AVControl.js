c5dc3532478e524a33d90a61fe68033718bbd1bf
Manmohit Matharu <manmohit.matharu@gmail.com>
2022-06-19 16:59:28 -0400

    add twilio avcontrol, track components

---
+++ client/src/components/AVControl.js
@@ -0,0 +1,21 @@
+import './App.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMicrophone, faMicrophoneSlash, faVideo, faVideoSlash } from '@fortawesome/free-solid-svg-icons';
+
+function AVControl(props) {
+  let icon;
+
+  if (props.trackOff) {
+    icon = props.type === 'audio' ? faMicrophoneSlash : faVideoSlash;
+  } else {
+    icon = props.type === 'audio' ? faMicrophone : faVideo;
+  }
+
+  return (
+    <div className='avControl'>
+      <FontAwesomeIcon icon={icon} onClick={() => props.toggleTrac()} />
+    </div>
+  )
+}
+
+export default AVControl;
